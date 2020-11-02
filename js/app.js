96131f1c2fa9bb9cd53b3fafb14502e5301c7868
ServeIron <servetgkdmr@gmail.com>
2020-11-02 12:50:33 +0300

    değişken adları düzeltildi

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ const app = new Vue({
         ['','',''],
         ['','','']
       ],
-      gameOwer : false,
-      sira : true,
+      gameOver : false,
+      desk : true,
       counter : 1,
-      gameBraing : false,
+      gameBrain : false,
       winners : '',
           
     },
@@ -18,39 +18,39 @@ const app = new Vue({
         let columnX;
         let columnY;       
         if (this.counter == 4) {
-            this.gameOwer = true;
-            this.gameBraing = null;
+            this.gameOver = true;
+            this.gameBrain = null;
         }
   
-        if(this.sira && !this.gameOwer){
+        if(this.desk && !this.gameOver){
             this.tac[indis][val] = 'X';
-            this.sira = false;      
-            this.gameBraing = true;   
+            this.desk = false;      
+            this.gameBrain = true;   
         } 
         for(let i = 0; i <= 2; i++){         
         //    x koordinat kontrolü        
             if(this.tac[i][0] == 'X' && this.tac[i][1] == 'X' && this.tac[i][2] == 'X'){
                 if(this.counter == 4){
-                this.gameOwer = true;
+                this.gameOver = true;
             }
                 this.winners = 'X';           
             }
         if(this.tac[i][0] == 'O' && this.tac[i][1] == 'O' && this.tac[i][2] == 'O'){
                 if(this.counter == 4){
-                this.gameOwer = true;
+                this.gameOver = true;
             }
                 this.winners = 'O';           
             }        
          //  y koordinat kontrolü
             if(this.tac[0][i] == 'X' && this.tac[1][i] == 'X' && this.tac[2][i] == 'X'){
                 if(this.counter == 4){
-                this.gameOwer = true;
+                this.gameOver = true;
             }
                 this.winners = 'X';           
             }
           if(this.tac[0][i] == 'O' && this.tac[1][i] == 'O' && this.tac[2][i] == 'O'){
                 if(this.counter == 4){
-                this.gameOwer = true;
+                this.gameOver = true;
             }
                 this.winners = 'O';           
             }
@@ -60,26 +60,26 @@ const app = new Vue({
        // X*Y koordinat kontrolü, bunuda elimizle yapalım :D           
        if(this.tac[0][0] == 'X' && this.tac[1][1] == 'X' && this.tac[2][2] == 'X'){
         if(this.counter == 4){
-            this.gameOwer = true;
+            this.gameOver = true;
         }
             this.winners = 'X';          
        }
         if(this.tac[0][2] == 'X' && this.tac[1][1] == 'X' && this.tac[2][0] == 'X'){
         if(this.counter == 4){
-            this.gameOwer = true;
+            this.gameOver = true;
         }
             this.winners = 'X';          
        }
     
         if(this.tac[0][0] == 'O' && this.tac[1][1] == 'O' && this.tac[2][2] == 'O'){
         if(this.counter == 4){
-            this.gameOwer = true;
+            this.gameOver = true;
         }
             this.winners = 'O';          
        }
         if(this.tac[0][2] == 'O' && this.tac[1][1] == 'O' && this.tac[2][0] == 'O'){
         if(this.counter == 4){
-            this.gameOwer = true;
+            this.gameOver = true;
         }
             this.winners = 'O';          
        }
@@ -89,8 +89,8 @@ const app = new Vue({
        }
     
        
- // Bilgisyara rastgele bir kutu seçtiriyoruz, iki şarta bakıyor 1.si girilen kutuyla aynı mı, veya boş mu eğer bu şartlar varsa tekrar kontrol edip uygun kutuyu buluyor, 
- // daha zeki yapmak için girilen kutuların indislerini alıp ona göre şartlar belirlenebilir                                 
+ // Bilgisyara rastgele bir kutu seçtiriyoruz, iki şarta bakıyor. Birinci girilen kutuyla aynı mı veya boş mu eğer bu şartlar varsa tekrar kontrol edip uygun kutuyu buluyor. 
+ // Daha zeki yapmak için girilen kutuların indislerini alıp ona göre şartlar belirlenebilir                                 
             setTimeout(()=> {
                 do {
                      columnX = Math.floor(Math.random() * 3);
@@ -98,8 +98,8 @@ const app = new Vue({
                   } while (this.tac[columnX][columnY] != '' );      
         
                   this.tac[columnX][columnY] = 'O';
-                  this.sira = true;
-                  this.gameBraing = false;               
+                  this.desk = true;
+                  this.gameBrain = false;               
             },2000);                
         this.$forceUpdate();      
      },
